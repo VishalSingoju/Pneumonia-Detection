f169cdf1821f16b35250c8b3eecc873f40a6c59c
VishalSingoju <vsingoju17@gmail.com>
2025-02-22 01:54:17 +0530

    changes in Image Preprocessing for Model Input

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+model = load_model("pneumonia_model.keras")
+
+def predict(img_path):
+    img = image.load_img(img_path, target_size(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+
+    prediction = model.predict(img_array)
+    return "PNEUMONIA" if prediction[0] > 0.5 else "NORMAL"

7d1230ea1c68f75a8aef7865d6f62f6e176fd4ea
VishalSingoju <vsingoju17@gmail.com>
2025-02-21 19:26:55 +0530

    path of dataset

--- train.py
+++ train.py
@@ -3,3 +3,15 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+DATASET_PATH = "chest_xray/"
+
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+
+train_gen = datagen.flow_from_directory(
+    DATASET_PATH + "train/",
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary',
+    subset='training'
+)

8b8f13dc91c0fc46ef9d8533a439a76dbd9d7ed6
VishalSingoju <vsingoju17@gmail.com>
2025-02-21 19:15:40 +0530

    Image data Generator package updated

--- train.py
+++ train.py
@@ -3,4 +3,3 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-

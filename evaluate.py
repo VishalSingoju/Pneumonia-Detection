bdfef605c2c6dd51080c9d48af419f305b3c0d8d
VishalSingoju <vsingoju17@gmail.com>
2025-02-21 23:05:35 +0530

    Initializing packages for evaluation

---
+++ evaluate.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+sys.stdout.reconfigure(encoding='utf-8')
